f4642d90dbe70f3ae10832d47cb880049bf881e7
Ryan-kamau <ryankamau27@gmail.com>
2025-09-30 03:28:05 +0300

    updated feature

--- providing_crypto_key/key.py
+++ providing_crypto_key/key.py
@@ -1,6 +1,8 @@
 from cryptography.fernet import Fernet
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 key = Fernet.generate_key()
 
-with open("secret_key_2.txt", mode = "wb") as sec_file:
+with open(os.path.join(BASE_DIR, "secret_key_2.txt"), mode = "wb") as sec_file:
     sec_file.write(key)
